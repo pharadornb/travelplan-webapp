33cb1ef5b1b20f7b8de8763d85c6a02d9372f717
P066 <varapan.wat@gmail.com>
2021-02-09 15:45:07 +0700

    commit 9/2/2564 15.45

--- login.php
+++ login.php
@@ -10,13 +10,13 @@
 </head>
 <body>
 
-    <form action="login_sus.php" class="card">
+    <form action="login_sus.php" class="card" method="post" >
     
     <h2>Sign in</h2> 
     <i id="logo" class="far fa-user-circle"></i>
     <br /> <br /> 
-    <input class="textbox" type="text" placeholder="Username">
-    <input class="textbox" type="password" placeholder="Password">
+    <input class="textbox" type="text" placeholder="Username" name = "username">
+    <input class="textbox" type="password" placeholder="Password" name = "password">
     <input class="btn-submit" type="submit" value="Login">
 
     <p>Don't have account?<a href="signup.php">Sign up</a></p>
---
+++ login_sus.php
@@ -0,0 +1,42 @@
+
+
+<?php 
+   include 'include/config_db.php';
+
+    $user =$_POST['username'];
+    $pass =$_POST['password'];
+
+
+      // echo $sql;
+      // echo $rs;
+       
+      mysqli_query($conn,"SET names TIS620");
+      mysqli_query($conn,"SET character_set_result=tis620");
+      mysqli_query($conn,"SET character_set_client='tis620'");
+      mysqli_query($conn,"SET character_set_connection='tis620'");
+  
+      $sql = "SELECT * FROM users WHERE username = '$user' AND password = '$pass' ";
+      $result = mysqli_query($conn, $sql);
+     //echo $sql;
+    // echo $result;
+  
+  
+  if (mysqli_num_rows($result) > 0) {
+          while($row = mysqli_fetch_assoc($result)) {
+              if($row['user_type_id'] == 1) { 
+                  //$_SESSION['Username']=$row["cus_name"];
+                  //$_SESSION['customer_id']=$row["cus_id"];
+                  header("location:admin.php");
+                  
+              } else if($row['user_type_id'] == 2) {
+                 // $_SESSION['Username']=$row["cus_name"];
+                 // $_SESSION['customer_id']=$row["cus_id"];
+                  header("location:index.php");
+              }      
+          }
+  } else{
+      header("location:login_error.php");
+      }
+  
+  mysqli_close($con);
+  ?>

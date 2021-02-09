e2e96a29629245ec1175f4ccfa019d3b6180d4f6
P066 <varapan.wat@gmail.com>
2021-02-09 15:23:01 +0700

    commit 9/2/2564 15.22

--- login.php
+++ login.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TravelPlaning</title>
-    <link rel="stylesheet" href="StyleLogin.css">
+    <link rel="stylesheet" href="css/StyleLogin.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
 
 </head>
 <body>
 
-    <form action="***.php" class="card">
+    <form action="login_sus.php" class="card">
     
     <h2>Sign in</h2> 
     <i id="logo" class="far fa-user-circle"></i>
@@ -21,6 +21,6 @@
 
     <p>Don't have account?<a href="signup.php">Sign up</a></p>
     </form>
-   สวัสดี เราทำเอง
+
 </body>
 </html>

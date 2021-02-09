331e68c20399c612a06985bf4a5822d27d218cad
P066 <varapan.wat@gmail.com>
2021-02-09 18:55:36 +0700

    commit 9/2/2564 18.55

--- signup.php
+++ signup.php
@@ -11,10 +11,11 @@
 
     <!-- Bootstrap css -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
-    <link rel="stylesheet" href="StyleSignup.css">
+   
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <!-- Custom css -->
-    <link href="stylesheets/stylesheet.css" rel="stylesheet">
+    
+    <link rel="stylesheet" href="css/StyleSignup.css">
   </head>
 
   <body>

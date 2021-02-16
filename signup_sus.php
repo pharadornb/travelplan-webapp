bf769eb69257ca0d89d2a74af7c4edc2b1097ab7
P066 <varapan.wat@gmail.com>
2021-02-16 16:43:20 +0700

    update signup

--- signup_sus.php
+++ signup_sus.php
@@ -1,4 +1,16 @@
 <?php SESSION_START(); ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert-dev.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css">
+    </head>
+    <body>
+
+
 <?php 
    include 'include/config_db.php';
     
@@ -25,10 +37,28 @@
  
 
 
-     header("location:signup_success.php");
+     //header("location:signup_success.php");
      
  
   mysqli_close($conn);
   ?>
+
+<script>
+    setTimeout(function(){
+      swal({
+          title:"สมัครสมาชิกสำเร็จ",
+          text:"ระบบวางแผนการท่องเที่ยว",
+          type:"success"
+      },function(){
+          window.location = "login.php";
+      }) ; 
+    },1000) ;
+    </script>
+
+
+
+    
+  </body>
+  </html>
 
 

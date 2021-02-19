25f41ce76d22aa9b287db13043a75474c7f62483
Pharadorn Boonruam <61776315+bom2542@users.noreply.github.com>
2021-02-19 16:11:50 +0700

    Update 19/02/2564 16.11

--- index.php
+++ index.php
@@ -24,32 +24,7 @@
 <body>
 
     <!--navbar-->
-    <nav>
-        <ul class="menu">
-            <a href="index.html"><img src="images/logo-application.png" width="35px"></a>
-            <li class="logo" id="topic_bold"><a href="index.html">
-                    <font color="white">ระบบวางแผนการท่องเที่ยว | Travel Planing System</font>
-                </a></li>
-            <li class="item"><a href="#">
-                    <font color="white">หน้าแรก</font>
-                </a></li>
-            <li class="item"><a href="#">
-                    <font color="white">สถานที่ท่องเที่ยวน่าสนใจ</font>
-                </a></li>
-            <li class="item"><a href="#">
-                    <font color="white">สื่อประชาสัมพันธ์</font>
-                </a></li>
-            <li class="item"><a href="#">
-                    <font color="white">ติดต่อเรา</font>
-                </a></li>
-            <li class="item button secondary"><a href="#">
-                    <font color="white">เข้าสู่ระบบ</font>
-                </a></li>
-            <li class="toggle"><a href="#">
-                    <font color="white"><i class="fas fa-bars"></font></i>
-                </a></li>
-        </ul>
-    </nav>
+    <?php include 'php/navbar.php' ?> 
 
     <!-- img slide -->
     <div id="carousel1" class="carousel slide px-0" data-ride="carousel">
---
+++ php/navbar.php
@@ -0,0 +1,26 @@
+<nav>
+    <ul class="menu">
+        <a href="index.html"><img src="images/logo-application.png" width="35px"></a>
+        <li class="logo" id="topic_bold"><a href="index.html">
+                <font color="white">ระบบวางแผนการท่องเที่ยว | Travel Planing System</font>
+            </a></li>
+        <li class="item"><a href="#">
+                <font color="white">หน้าแรก</font>
+            </a></li>
+        <li class="item"><a href="#">
+                <font color="white">สถานที่ท่องเที่ยวน่าสนใจ</font>
+            </a></li>
+        <li class="item"><a href="#">
+                <font color="white">สื่อประชาสัมพันธ์</font>
+            </a></li>
+        <li class="item"><a href="#">
+                <font color="white">ติดต่อเรา</font>
+            </a></li>
+        <li class="item button secondary"><a href="#">
+                <font color="white">เข้าสู่ระบบ</font>
+            </a></li>
+        <li class="toggle"><a href="#">
+                <font color="white"><i class="fas fa-bars"></font></i>
+            </a></li>
+    </ul>
+</nav>

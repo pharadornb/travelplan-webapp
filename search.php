90bf8e607c72d487cc04f10d15108462cba9e6b0
P066 <varapan.wat@gmail.com>
2021-02-20 03:34:26 +0700

    commit search 20/2/2564 3.34

--- search.php
+++ search.php
@@ -1,43 +1,82 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Document</title>
+<!-- Bootstrap CSS -->
+<link rel="stylesheet"
+href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"crossorigin="anonymous">
+
 </head>
 <body>
-   <div class="container" style="width:900px;">
-   <h2 align="center">Search TravelPlaning</h2>
-   <div align="center">
-   <input type ="text" name="search" id="search"placeholder="Search" class="form-control"/>
-   </div>
-   <ul calss="list-group" id="result"></ul>  
-   </div>
-   <script type="text/javascript">  
-       $(document).ready(function(){//ดักจับ event  ตอนที่ผู้ใช้ป้อนข้อความมา 
-       
-          $('#search').keyup(function(){  //นำข้อมูลที่ป้อนใน textbox มาเก็บไว้ในตัวแปรใดตัวแปรหนึ่ง
-            $('#result').html('');
-                   var txt = $(this).val();    // ข้อมูลที่ผู้ใช้ทำการป้อนมาเก็บลงใน  ตัวแปร txt
-                  // alert(txt);
+<div class="container-fluid py-4">
+<h2 class="text-center mb-4">Search TravelPlaning</h2>
+<div class="row mb-4">
+<div class="col-12">
+<input type="text" name="search" id="search" placeholder="Search"class="form-control"onkeyup="search(this);"autocomplete="off">
 
-                  var expression =new RegExp(txt,"i");  //นำค่าที่อยู่ใน ตัวแปร txt มาเปรียบเทียบกับข้อมูลที่อยู่ใน json //กำหนด เคส *ในที่นี้ก็คือไม่กำหนดตัวพืมพ์เล็กหรือพืมพ์ใหญ่
-                 $.getJSON("travel_data.json",function(data){
-                     $.each(data,function(key,value){
-                      if(value.name.search(expression)!=-1 || value.location.search(expression)!=-1 ) //เปรียบเทียบ key name และ key location  ว่ามันตรงกับที่เรารับค่าใน text มาใหม่ ใน json   //ถ้าเป็น -1 ก็คือหาไม่เจอ
-                      {
-                         //แสดงข้อมูล
-                        
-                         $('#result').append('<li class="list-group-item link-class"><img src="img/AP1.jpg" height="40" width="40" class="img-thumbnail"/>'+value.name+'|<span class="text-muted">'+value.location+'</span></li>');
-                      }
-                     })
-                 })
-          });
-       });
-   </script>
+</div>
+</div>
+<div class="row" id="result">
+</div>
+</div>
+<!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"crossorigin="anonymous"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"
+integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns"crossorigin="anonymous"></script>
+<script type="text/javascript">
+
+$(document).ready(function () {//ดักจับ event ตอนท่ีผูใ้ ชป้้อนขอ้ ความมา
+});
+async function search(obj) {
+let elem = "";
+var formData = new FormData()
+formData.append("search", obj.value)
+const response = await
+
+fetch("http://192.168.64.2/TravelPlan2563/get_search.php", {
+method: 'POST', // *GET, POST, PUT, DELETE, etc.
+mode: 'cors', // no-cors, *cors, same-origin
+
+cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+
+credentials: 'include', // include, *same-origin, omit
+// headers: {
+// 'Content-Type': 'application/json'
+// // 'Content-Type': 'application/x-www-form-urlencoded',
+// },
+
+body: formData
+});
+
+const data = await response.json(); // รับค่า response ที่ส่งมามาใส่ไว้ในตัวแปร data
+
+
+if (data.result.length > 0) {
+data.result.map((item, index) => {
+elem += `<div class="col-12 mb-3">`;
+elem += `<div class="card">`;
+elem += `<div class="card-body">`;
+elem += `<p>${item.name}</p>`;
+elem += `<a href="tourist_attractions?id=${item.id}" class="btn
+
+btn-primary" >See detail</a>`;
+elem += `</div>`;
+elem += `</div>`;
+elem += `</div>`;
+})
+} else {
+elem += `<div class="col-12 mb-3">`;
+elem += "<h4 class='text-center'>Data not found!</h4>"
+elem += `</div>`;
+}
+$("#result").empty().append(elem)
+}
+</script>
 </body>
 </html>

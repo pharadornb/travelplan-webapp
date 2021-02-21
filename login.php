9598e572963670b64653d728da90bacb200c6b63
Pharadorn Boonruam <61776315+bom2542@users.noreply.github.com>
2021-02-21 14:40:18 +0700

    Update 21/02/2564 14.40

--- login.php
+++ login.php
@@ -1,37 +1,34 @@
-<?php
-// Start the session
-session_start();
-?>
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>[เข้าสู่ระบบ]-ระบบวางแผนท่องเที่ยว</title>
-    <!--css cdn-->
-    <link rel="stylesheet" href="css/styleLoginF.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="javascript/showphoto.js"></script>
-</head>
-<body class="bg-lg">
-    <center>
-    <div class="ct">
-   <img src="images/logo/logo-application.png" alt="" width="120px" height="150px" style="margin-top: 40px;">
 
-        <form action="login_sus.php" method="POST">
-            <div style="margin-top: 10%;">
-                <h4>+เข้าสู่ระบบวางแผนท่องเที่ยว+</h4>
-            </div>
-            <div style="margin-top: 10%;">
-                <p>ชื่อผู่้ใช้</p>
-                <input type="text" name="username" id="username" placeholder="Username" maxlength="15" required>
-                <br><br>
-                <p>รหัสผ่าน</p>
-                <input type="password" name="password" id="password" placeholder="Password" maxlength="20" required>
-                <br><br>
-                <input type="submit" value="Login" name="bt-login" class="bt-lg"><br><br>
-                <a href="register1.php"><u>ยังไม่มีชื่อบัญชีผู้ใช้?</u></a>
-                <br><br><br>
-            </div>
+    <!-- css -->
+    <?php include 'php/header.php' ?>
+
+</head>
+<body>
+<div class="mt-5" align="center">
+    <img src="images/logo/logo-application.png" alt="" width="120px" height="150px">
+
+    <form action="login_sus.php" method="POST" style="max-width: 300px; margin: auto;">
+        <div class="form-group">
+            <h4 class="d-flex justify-content-center font-weight-bold mt-4"> + เข้าสู่ระบบวางแผนท่องเที่ยว + </h4>
+            <input type="text" class="form-control mt-4" name="username" id="username" placeholder="ชื่อผู้ใช้"
+                   maxlength="15" required>
+            <input type="password" class="form-control mt-2" name="password" id="password" placeholder="รหัสผ่าน"
+                   maxlength="20" required>
+            <center><input type="submit" value="Login" name="bt-login"
+                           class="bt-lg btn btn-primary mt-3 mb-2 btn-block"></center>
+            <a href="register1.php"><u>ยังไม่มีชื่อบัญชีผู้ใช้?</u></a>
+        </div>
     </form>
-    </div>
-    </center>
+</div>
+
+<?php include 'php/script.php'?>
+</body>
+</html>

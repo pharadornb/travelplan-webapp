860d8600c091e3126f26ace3590fb25439dc5844
Chidchansa <72435106+flockjoin@users.noreply.github.com>
2021-02-21 13:31:25 +0700

    commit 13:31 21/2/2564

--- register1.php
+++ register1.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,113 +16,76 @@
         <div class="rg-div-1">
             <img src="images/logo/logo-application.png" alt="" width="120px" height="150px" style="margin-top: 40px;">
             <br><br><h3>+ สมัครสมาชิกระบบวางแผนท่องเที่ยว +</h3><br><br>
-            <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST">
+            <form action="register_sus.php" method="POST">
                 <p>ชื่อ-สกุล:</p>
                 <input type="text" name="fullname" id="fullname" placeholder="Fullname" class="ds-01" required> 
                 <br>
-                <p id="fn"></p>
+                <?php if(isset($_SESSION['error_fullname'])) : ?>
+                    <br>
+                    <div class="nf">
+                            <?php
+                                echo $_SESSION['error_fullname'];
+                                unset($_SESSION['error_fullname']);
+                            ?>
+                    </div>
+                <?php endif ?>
+                <br>
                 <p>วัน/เดือน/ปีเกิด:</p>
                 <input type="date" name="birthday" id="birthday" required>
                 <br><br>
                 ชื่อผู้ใช้:
                 <br><br>
                 <input type="text" name="username" id="username" class="ds-01" maxlength="15" placeholder="Username"  required>
-                <p id="fun"></p>
+                <br>
+                <?php if(isset($_SESSION['error_username'])):?>
+                    <br>
+                    <div class="nf">
+                            <?php
+                                echo $_SESSION['error_username'];
+                                unset($_SESSION['error_username']);
+                            ?>
+                    </div>
+                <?php endif ?>
+                <br>
                 รหัสผ่าน:
                 <br><br>
                 <input type="password" name="password" id="password" class="ds-01" placeholder="Password" required>
-                <p id="fp"></p>
+                <br>
+                <?php if(isset($_SESSION['error_password'])):?>
+                    <br>
+                    <div class="nf">
+                            <?php
+                                echo $_SESSION['error_password'];
+                                unset($_SESSION['error_password']);
+                            ?>
+                    </div>
+                <?php endif ?>
+                <br>
                 ยืนยันรหัสผ่าน:
                 <br><br>
                 <input type="password" name="confrimpassword" id="confrimpassword" class="ds-01" placeholder="Confrim Password" required>
-                <p id="fpc"></p>
+                <br>
+                <?php if(isset($_SESSION['error_confrimpassword'])):?>
+                    <br>
+                    <div class="nf">
+                            <?php
+                                echo $_SESSION['error_confrimpassword'];
+                                unset($_SESSION['error_confrimpassword']);
+                            ?>
+                    </div>
+                <?php endif ?>
+                <br>
                 Email:
                 <br><br>
                 <input type="email" name="email" placeholder="E-mail" class="ds-01" id="email" required>
                 <br><br>
-                <input type="file" name="photo" id="photo" accept="image/png, image/jpeg">
+                <input type="file" name="image" id="image" accept="image/png, image/jpeg">
                 <br><br>
                 <input type="submit" value="สมัครสมาชิก" name="bt-rg1" id="bt-rg1">
                 <br><br>
         </form>
         </div>
     </center>
-    <?php
-   
-    $hostname = "mysql-19614-0.cloudclusters.net:19614";
-    $username = "TravelPlan2021";
-    $password = "jYtKQ2Y1VZz1";
-    $database = "TravelPlan2021";
-
-    //Connect dB
-    $conn = new mysqli($hostname, $username, $password, $database);
-    $conn->query("SET NAMES UTF8");
-
-    //Check dB die
-    if ($conn->connect_error) {
-        die('Could not connect: ' . $conn->connect_error);
-    }
-
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $fullname = $_REQUEST['fullname'];
-            $birthday = $_REQUEST['birthday'];
-            $username = $_REQUEST['username'];
-            $password = $_REQUEST['password'];
-            $confrimpassword = $_REQUEST['confrimpassword'];
-            $email = $_REQUEST['email'];
-
-             
-
-            $n = strlen($fullname);
-            $pattern = "/[ก-์\s]/";
-            $u = preg_match_all($pattern, $fullname);
-
-
-            $sql = "SELECT (username) FROM user WHERE username = '$username'";
-            $result = mysqli_query($conn, $sql);
-
-
-            //Check thai in fullname
-            if($n==$u){
-                //Check if the username is in the database or not.
-                if(mysqli_num_rows($result) == 0){
-                    //Check Number of characters > 8
-                    if(strlen($password) >= 9){
-                        //Check Letters in Password
-                        if(preg_match_all('/[A-z]/',  $password) >= 1 || preg_match_all('/[ก-ฮ]/',  $password) >= 1){
-                            //Check password = confrimpassword
-                            if(strcmp($password, $confrimpassword)==0){
-                                //------
-                            }else{
-                                echo "<script>";
-                                echo "document.getElementById('fpc').innerHTML = '<p><div id=\"nf\">รหัสผ่านไม่ตรงกัน</div></p>';";
-                                echo "</script>";
-                            }
-                        }else{
-                            echo "<script>";
-                            echo "document.getElementById('fp').innerHTML = '<p><div id=\"nf\">กรุณาใส่ตัวอักษรผสมเข้าไปด้วย</div></p>';";
-                            echo "</script>";
-                        }
-                    }else{
-                        echo "<script>";
-                        echo "document.getElementById('fp').innerHTML = '<p><div id=\"nf\">กรุณากรอกให้เกิน 8 ตัวอักษร</div></p>';";
-                        echo "</script>";
-                    }
-                }else{
-                    echo "<script>";
-                    echo "document.getElementById('fun').innerHTML = '<p><div id=\"nf\">มีชื่อผู้ใช้อยู่แล้ว</div></p>';";
-                    echo "</script>";
-                }
-            }else{    
-                echo "<script>";
-                echo "document.getElementById('fn').innerHTML = '<p><div id=\"nf\">กรุณากรอกภาษาไทย</div></p>';";
-                echo "</script>";
-            } 
-
-        }
-
-        $conn->close();
-    ?>
     <!--script cdn--> 
     <!-- <script src="script/scriptregister.js"></script> -->
     <script href="script/script.js"></script>

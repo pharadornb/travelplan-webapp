e9cb7118b7dcb94cab3f9209790f5b8f97162a21
Pharadorn Boonruam <61776315+bom2542@users.noreply.github.com>
2021-02-24 12:59:23 +0700

    Successfully coding 24/2/2564 12.59

--- admin/dbconnect.php
+++ admin/dbconnect.php
@@ -1,17 +1,17 @@
 <?php
     // clounddb
     
-    /*$hostname = "mysql-19614-0.cloudclusters.net:19614";
+    $hostname = "mysql-19614-0.cloudclusters.net:19614";
     $username = "TravelPlan2021";
     $password = "jYtKQ2Y1VZz1";
-    $database = "TravelPlan2021";*/
+    $database = "TravelPlan2021";
 
 
     // localhost
-    $hostname = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "TravelPlan2021";
+//    $hostname = "localhost";
+//    $username = "root";
+//    $password = "";
+//    $database = "TravelPlan2021";
 
 
     //Connect dB
--- php/dbconnect_object.php
+++ php/dbconnect_object.php
@@ -1,16 +1,16 @@
 <?php
     // clounddb
     
-//        define('DB_SERVER', 'mysql-19614-0.cloudclusters.net:19614');
-//        define('DB_USER', 'TravelPlan2021');
-//        define('DB_PASS', 'jYtKQ2Y1VZz1');
-//        define('DB_NAME', 'TravelPlan2021');
+        define('DB_SERVER', 'mysql-19614-0.cloudclusters.net:19614');
+        define('DB_USER', 'TravelPlan2021');
+        define('DB_PASS', 'jYtKQ2Y1VZz1');
+        define('DB_NAME', 'TravelPlan2021');
     
 
     // localhost
-        define('DB_SERVER', 'localhost');
-        define('DB_USER', 'root');
-        define('DB_PASS', '');
-        define('DB_NAME', 'TravelPlan2021');
+//        define('DB_SERVER', 'localhost');
+//        define('DB_USER', 'root');
+//        define('DB_PASS', '');
+//        define('DB_NAME', 'TravelPlan2021');
 
 ?>
